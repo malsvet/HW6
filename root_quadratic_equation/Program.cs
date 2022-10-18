927178ea17c8195387a1a6e1a8d64c86ae5750c4
malsvet <47292391+malsvet@users.noreply.github.com>
2022-10-19 00:58:52 +0700

    Add files via upload

---
+++ root_quadratic_equation/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа вычисляет корни квадратного уравнения.
+
+Console.WriteLine("----------------------------------------------------------");
+Console.WriteLine("----- Программа вычисляет корни квадратного уранения -----");
+Console.WriteLine("-------------------- вида ax^2+bx+c=0 --------------------");
+Console.WriteLine("----------------------------------------------------------");
+Console.WriteLine("                                                          ");
+Console.Write("Введите a: ");
+double a = Double.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+double b = Double.Parse(Console.ReadLine());
+Console.Write("Введите c: ");
+double c = Double.Parse(Console.ReadLine());
+double D = Math.Pow(b, 2)-4*a*c;
+if (D < 0) Console.WriteLine("Уравнение не имеет корней");
+else if (D == 0)
+{
+    double x = -b / (2 * a);
+    Console.WriteLine($"Корень уравнения {x}");
+}
+else
+{
+    double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+    double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+    Console.WriteLine($"Первый корень уравнения {Math.Round(x1, 3)}");
+    Console.WriteLine($"Второй корень уравнения {Math.Round(x2, 3)}");
+}
+Console.ReadKey();
